f5c9376ce0aa4be36b8f3921022065b6ab91e7f4
digitechgrouperu <digitechgrouperu@gmail.com>
2025-06-26 18:33:00 -0500

    upd

--- app/Exports/DelaysExport.php
+++ app/Exports/DelaysExport.php
@@ -4,14 +4,41 @@ namespace App\Exports;
 
 use App\Models\Delay;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class DelaysExport implements FromCollection
+class DelaysExport implements FromCollection, WithHeadings
 {
     /**
-    * @return \Illuminate\Support\Collection
-    */
+     * @return \Illuminate\Support\Collection
+     */
     public function collection()
     {
-        return Delay::all();
+        return Delay::with('vehicle', 'reason', 'user')
+            ->select('id', 'user_id', 'vehicle_id', 'reason_id', 'status', 'start_time', 'end_time')
+            ->get()
+            ->map(function ($delay) {
+                return [
+                    'ID' => $delay->id,
+                    'Usuario' => $delay->user->name ?? 'N/A',
+                    'Vehículo' => $delay->vehicle->code ?? 'N/A',
+                    'Razón' => $delay->reason->description ?? 'N/A',
+                    'Estado' => $delay->status,
+                    'Hora de Inicio' => $delay->start_time,
+                    'Hora de Fin' => $delay->end_time,
+                ];
+            });
+    }
+
+    public function headings(): array
+    {
+        return [
+            'ID',
+            'Usuario',
+            'Vehículo',
+            'Razón',
+            'Estado',
+            'Hora de Inicio',
+            'Hora de Fin',
+        ];
     }
 }
